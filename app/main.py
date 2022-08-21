4cdda61df0d0a2261282e03d108acac0d7f5999e
Apel Mahmud <44221242+apl-mhd@users.noreply.github.com>
2022-08-21 17:01:24 +0600

    fastapi chat app

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+from http import client
 from typing import List
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi import Request
+import json
 
 
 
@@ -22,14 +24,14 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: str, websocket: WebSocket, id:int):
+        msg = json.dumps({'id':'', 'message':message })
+        await websocket.send_json(json.loads(msg))
 
-    async def broadcast(self, message: str):
-        print(self.active_connections)
+    async def broadcast(self, message: str, id:str):
         for connection in self.active_connections:
-            await connection.send_text(message)
-
+            msg = json.dumps({'id':id, 'message':message})
+            await connection.send_json(json.loads(msg))
 
 manager = ConnectionManager()
 
@@ -45,8 +47,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
+            await manager.send_personal_message(f"You wrote: {data}", websocket, client_id)
+            await manager.broadcast(f"Client  says: {data}", client_id)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
+        await manager.broadcast(f"Client left the chat", client_id)
